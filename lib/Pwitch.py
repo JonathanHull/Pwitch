38d0d286b71379ce4b202095a230bb6b633961ff
Jonathan Hull <jonathan.hull11@gmail.com>
2018-07-12 01:20:19 +0100

    Fixed timeout problem when calling for admin list

--- lib/Pwitch.py
+++ lib/Pwitch.py
@@ -2,13 +2,16 @@
 
 import socket
 import readline
+import select
 import re
 import sys
 
 from multiprocessing import Process
 from time import sleep
 
-## Command line argument parser.
+## incorporate loyalty points
+## Add method to log mod command useage.
+
 
 ## Create multithreading wrapper
 def threaded(func):
@@ -23,12 +26,13 @@ class Pwitch:
                 username, 
                 oauth, 
                 ircRoom, 
-                updateRate=(20/30),
+                updateRate=(10/30),
                 verbose=False,
                 host="irc.twitch.tv",
                 port=6667,
                 logging=False,
-                chatCommands=None
+                chatCommands=None,
+                loyaltyMode=False
                 ):
 
         self.username = username
@@ -40,8 +44,10 @@ class Pwitch:
         self.port = port
         self.logging = logging
         self.chatCommands = chatCommands
-        self.connected = True
 
+        self.mod_only_mode = False
+
+        self.connected = True
         self.sock = self.connectIRC()
 
         if logging:
@@ -49,6 +55,8 @@ class Pwitch:
 
         """
         Pwitch
+
+        Parent Pwitch package class.
 
         Parameters:-
         :param username:   Twitch account username.
@@ -58,10 +66,12 @@ class Pwitch:
         :param verbose:    Turn on verbose output : Boolean.
         :param host:       The Twitch IRC sever (irc.twitch.tv).
         :param port:       Port to connect to server (6667).
-        :param logging:    Toggle chat logging (Default False).
+        :param logging:    Toggle chat logging (Default False). Options are:
+            - True (Log to log/#IRCROOM).
+            - FILE (log to specified file).
 
         Note: verbose/logging False by default.
-        Note: updateRate default = 20/30 (20 commands per 30 secs).
+        Note: updateRate default = 10/30 (30 commands per 10 secs).
 
         Usage: x = Pwitch("Twitch_username", "oauth_token", "#ircRoom", True|False)
                x.ban("Twitch_username")
@@ -85,14 +95,19 @@ class Pwitch:
         s.send("PASS {}\r\n".format(self.oauth).encode("utf-8"))
         s.send("NICK {}\r\n".format(self.username).encode("utf-8"))
         s.send("JOIN {}\r\n".format(ircRoom).encode("utf-8"))
+        s.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
+        s.send("CAP REQ :twitch.tv/tags\r\n".encode("utf-8"))
+        s.send("CAP REQ :twitch.tv/membership\r\n".encode("utf-8"))
         if s.recv(1024).decode("utf-8") and self.verbose:
             print("Username: {}\nChannel: {}\n".format(self.username,
                 ircRoom.lstrip('#')))
 
+        s.settimeout(None)
+
         return s
 
     ##@threaded
-    def updateIRC(self, sock):
+    def updateIRC(self, sock=None):
         """
         updateIRC
         
@@ -102,42 +117,73 @@ class Pwitch:
         paramers:-
         :param sock:   Socket object returned from connectIRC.
         """
-        ## Active connection with Twitch irc sever.
+
+        if not sock:
+            sock=self.sock
 
         while self.connected:
 
-            response = sock.recv(1024).decode("utf-8")
+            ready = select.select([sock], [], [], self.updateRate)
+
+            if ready[0]:
+                response = sock.recv(1024).decode("utf-8")
+            else:
+                continue
             if response == "PING :tmi.twitch.tv\r\n":
                 sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-        
-            else:
-                name = re.match(r'^:(.*)![^:]*:(.*)', response, re.M|re.I)
-                if name:
 
-                    ## Stores user input to buffer; stops irc chat overwriting
-                    ## terminal input.
-                    sys.stdout.write('\r'+' '*(len(readline.get_line_buffer())+2)+'\r')
-                    if self.verbose:
-                        print("{}: {}".format(name.group(1), name.group(2)))
+            else:
+                name = re.search('broadcaster/(\d).*mod=(\d).*:(.*)!.*:+?(.*)',
+                        response, re.I|re.M)
+                notice = re.search('NOTICE.*:+?(.*)', response, re.M|re.I)
+
+                print(response)
+
+                if self.verbose:
+                    if name:
+                        ## Store user input into buffer; stop print from
+                        ## overwriting terminal input.
+                        ## Note: Careful if implement GUI.
+                        sys.stdout.write('\r'+' '*(len(readline.get_line_buffer())
+                                +len(self.username)+2)+'\r')
+
+                        ## Check whether user is a mod or the broadcaster.
+                        ## Need to check modlist instead
+                        ## if self.ircRoom == name then B
+
+                        if int(name.group(1)) == 1:
+                            print("[B] {}: {}".format(name.group(3),
+                                name.group(4)))
+                        elif int(name.group(2)) >  1:
+                            print("[M] {}: {}".format(name.group(3),
+                                name.group(4)))
+                        elif not self.mod_only_mode:
+                            print("{}: {}".format(name.group(3), name.group(4)))
+
+                    if notice:
+                        print(notice.group(1))
 
                     if self.logging:
-                        pass
+                        if self.logging == True:
+                            pass
+                        else:
+                            pass
+
+
+
 
                     ## Detect !commands.
-                    if name and re.match(r'^!.*$', name.group(2), re.M|re.I):
-                        print("Disconnecting...")
-                        self.connected = False
+                    #if name and re.match(r'^!.*$', name.group(2), re.M|re.I):
+                    #    print("Disconnecting...")
+                    #    self.connected = False
 
                     ## Restores user input from buffer.
                     sys.stdout.write("{}: {}".format(self.username,
                         readline.get_line_buffer()))
                     sys.stdout.flush()
 
-            sleep(self.updateRate)
 
-
-    def getAdmins(self):
-        """Get a list of admins"""
+    def createThread(self):
         pass
 
     def loadCommands(self):
@@ -149,32 +195,134 @@ class Pwitch:
         pass
 
     def logChat(self):
-        """Start logging chat"""
-        pass
+        """Start logging chat"""j
+        self.log = open(self.ircRoom, 'a+')
 
-    def chat(self, message):
+    
+    def getMods(self):
+        """Get a list of admins"""
+        new_socket = self.connectIRC()
+        n=None
+
+        while not n:
+            ready = select.select([new_socket], [], [], self.updateRate)
+
+            if ready[0]:
+                response = new_socket.recv(1024).decode("utf-8")
+            else:
+                continue
+            if response == "PING :tmi.twitch.tv\r\n":
+                sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+
+            self.chat(".mods", socket=new_socket)
+
+            n = re.search(r':\s(.*)', response, re.I|re.M)
+
+            #try:
+            #    n = re.search(r':\s(.*)', response, re.I|re.M).group(1)
+            #except:
+            #    print("no")
+
+        print("Mods: {}".format(n))
+
+
+    def chat(self, message, socket=None):
         """Send message to twitch irc room."""
-        self.sock.send("PRIVMSG {} :{}\r\n".format(self.ircRoom, message).encode("utf-8"))
+        if not socket:
+            socket = self.sock
+
+        socket.send("PRIVMSG {} :{}\r\n".format(self.ircRoom, message).encode("utf-8"))
 
     def whisper(self, user, message):
         """Send a whisper to the specified user."""
-        self.chat(".w {} {}".format(user, message).format("utf-8"))
+        #self.chat(".w {} {}".format(user, message))
+
+        #self.sock.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
+        #self.sock.send("PRIVMSG {} :{}\r\n".format(user,
+        #    message).encode("utf-8"))
+
+        #while True:
+
+        #    ready = select.select([self.sock], [], [], self.updateRate)
+        #    if ready[0]:
+        #        response = self.sock.recv(1024).decode("utf-8")
+        #    else:
+        #        continue
+        #    print(response)
+
+        #if ready[0]:
+        #    response = sock.recv(1024).decode("utf-8")
 
     def ban(self, user):
         """Ban the specified user from the current channel."""
-        self.chat(".ban {}".format(user).encode("utf-8"))
+        self.chat(".ban {}".format(user))
 
     def unban(self, user):
         """Unban the specified user from the current channel."""
-        self.chat(".unban {}".format(user).encode("utf-8"))
+        self.chat(".unban {}".format(user))
 
     def timeout(self, user, secs=600):
         """Timeout the specified user for the given number of seconds."""
-        self.chat(".timeout {} {}".format(user, secs).encode("utf-8"))
+        self.chat(".timeout {} {}".format(user, secs))
 
     def untimeout(self, user):
         """Remove timeout for the specified user."""
-        self.chat(".untimeout {}".format(user).encode("utf-8"))
+        self.chat(".untimeout {}".format(user))
+
+    def slow(self, duration):
+        """Limit how often users may send messages, in seconds."""
+        self.chat(".slow {}".format(duration))
+
+    def slowoff(self):
+        """Disable slowmode."""
+        self.chat(".slowoff")
+
+    def clear(self):
+        """Clear chatroom."""
+        self.chat(".clear")
+        print("Chat was cleared by a moderator")
+
+    def subscribers(self):
+        """Turn on subscribers only mode."""
+        self.chat(".subscribers")
+
+    def subscribersoff(self):
+        """Turn off subscribers only mode."""
+        self.chat(".subscribersoff")
+
+    def followerMode(self, duration):
+        """Turn on followers only mode."""
+        self.chat(".followers {}".format(duration))
+
+    def followerModeOff(self):
+        """Turn off followers only mode."""
+        self.chat(".followersoff")
+
+    def host(self, channel, *tagline):
+        """Host the specified channel."""
+        self.chat(".host {} {}".format(channel, tagline[0]))
+
+    def unhost(self):
+        """Stop hosting channels."""
+        self.chat(".unhost")
+
+    def commercial(self, *duration):
+        """Start commercials for x number of seconds"""
+        self.chat(".commercial {}".format(duration[0]))
+
+    def mod(self, user):
+        """Grant mod status to a user."""
+        self.chat(".mod {}".format(user))
+
+    def unmod(self, user):
+        """Revoke mod status from a user."""
+        self.chat(".unmod {}".format(user))
+
+    def mod_list(self):
+        self.chat(".mods")
+
+
+
 
 
 ## Create multithreading wrapper
