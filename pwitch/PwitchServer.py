090abdcd1a3795948def30ccdc50075ec8c53cf1
Jonathan Hull <jonathan.hull11@gmail.com>
2018-09-17 19:54:02 +0100

    Enabled piping between threads, added watchdog method

--- pwitch/PwitchServer.py
+++ pwitch/PwitchServer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3.6
 
-import json
-import queue
-import multiprocessing as mp
+import time
 
-from time import sleep
 from .Pwitch import Pwitch
+from threading import Thread
 from multiprocessing import Process, Queue
 
 ## Enable users to check status of each thread if input is enabled
@@ -28,32 +26,62 @@ class PwitchServer:
         simultaneously.
 
         Parameters:-
-        :param cfg:         cfg dictionary - Check cfg/cfg.json
-        :param channels:    List of irc channels to monitor - Check
-                            cfg/irc_channels.json
-        :param timeout:     Set the 
+        :param cfg:             cfg dictionary - Check cfg/cfg.json
+        :param channels:        List of irc channels to monitor - Check
+                                cfg/irc_channels.json
+        :param timeout:         Set the 
         """
 
         self.cfg = cfg
         self.channels = channels
 
-    def watchdog(self):
+    def start(self):
+        """
+        start
+
+        Initialises and starts all Pwitch processes.
+        """
+        self.process_dict = self._create_process_dict()
+        self.start_processes(self.process_dict)
+
+        ## Watchdog thread -- Monitors process status.
+        watchdog_thread = Thread(target=self.watchdog)
+        watchdog_thread.start()
+
+
+    def watchdog(self, timeout=10):
         """
         watchdog
 
         Watches the queue object for updates, and sends a signal to it if the
         status of the child process has changed
+
+        Paramters:-
+        :param timeout:         Time for process to shutdown before termination.
         """
+        ## Extensive logging required.
         while True:
-            try:
-                msg = q.get(timeout=self.timeout)
+            for process,queue,flag in self.process_dict.values():
 
-            except:
-                pass
+            ## try to terminate if attempt to close failed.
+                if not flag: 
+                    process.terminate()
+                    time.sleep(0.1)
+                    if not process.is_alive():
+                        process.join(timeout=1.0)
+                        queue.close()
+                        self.process_dict.pop(i)
+                        continue
 
-    def _generateThreadDict(self):
+                if not process.is_alive():
+                    ## restart dead process
+                    pass
+
+                time.sleep(0.1)
+
+    def _create_process_dict(self):
         """
-        _generateThreadList
+        create_process_dictionary
 
         Generates dictionary containing Pwitch obejcts, and process object.
         e.g. adict["ircRoom"] = [PwitchObject, Process]
@@ -67,42 +95,64 @@ class PwitchServer:
 
         for i in channels:
             q = Queue()
-            #pwitch_object = Pwitch(username, oauth, i)
             a = Pwitch(username, oauth, i, process_queue=q)
-            q.put(True)
-            adict[i] = [Process(target=a.start), q]
-            #adict[i] = [Process(target=Pwitch, args=(username, oauth, i,
-            #    child_conn)), parent_conn]
-            #adict[i] = [pwitch_object, Process(target=pwitch_object.start)]
-
+            adict[i] = [Process(target=a.start), q, True]
 
         return adict
 
-    def startThreads(self):
+    def add_process(self, username, oauth, ircRoom):
         """
-        startThreads
+        add_process
 
-        Helper function; intialises pwitch threads
+        Paramters:-
+        :param username:        bot username.
+        :param oauth:           bot oauth key.
+        :param ircRoom:         Channel for Pwitch to monitor.
+        """
+        q = Queue()
+        a = Pwitch(username, oauth, i, process_queue=q)
+
+        ## Note: Final True is a flag for the watchdog method.
+        self.process_dict[ircRoom] = [Process(target=a.start), q, True]
+
+    def start_processes(self, process_dict):
+        """
+        start_processes
+
+        Helper function; starts all Pwitch processes.
+
+        Parameters:-
+        :param process_dict:    Dictionary of Pwitch processes generated from
+                                create_process_dict.
+                                i.e. adict["ircRoom"] = [Process, Queue]
         """
         print("[SERVER] Starting...")
-
-        self.adict = self._generateThreadDict()
-
-        for i in self.adict.keys():
-            self.adict[i][0].start()
+        for i in process_dict.keys():
+            process_dict[i][0].name = i
+            process_dict[i][0].start()
             print("[CONNECTED] {}".format(i.lstrip("#")))
 
-    def stopThreads(self):
+    def stop_process(self, pwitch_process):
         """
-        stopThreads
+        stop_process
+
+        Parameters:-
+        :param pwitch_process:         list containing process and Queue.
+        """
+
+        pwitch_process[1].put(False)
+        ## Watchdog to make sure it shuts down
+
+    def stop_processes(self):
+        """
+        stop_threads
 
         Helper function; stops all pwitch threads
         """
-        
         try:
-            self.adict_keys()
+            self.process_dict
         except:
-            print("Processes have not been initalised.")
+            print("Processes may not have been initalised.")
         else:
             for i in self.adict.keys():
                 self.adict[i][0].put(False)
@@ -111,6 +161,7 @@ class PwitchServer:
         ## Join threads
 
 if __name__ == "__main__":
+     import json
      cfg_dir = "/home/jonathan/projects/Pwitch/cfg/"
      cfg = cfg_dir+"cfg.json"
      channels = cfg_dir+"irc_channels.json"
@@ -122,4 +173,3 @@ if __name__ == "__main__":
 
      ob = PwitchServer(bot_cfg, channel_list)
      ob.startThreads()
-
