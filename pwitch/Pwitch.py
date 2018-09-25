624156256b10a12b40a4518c0b54b5659a2444ff
Jonathan Hull <jonathan.hull11@gmail.com>
2018-09-25 03:04:07 +0100

    Improved user command handling - need to implement permissions

--- pwitch/Pwitch.py
+++ pwitch/Pwitch.py
@@ -3,11 +3,13 @@
 import socket
 import select
 import readline # Remove - Use ncurses instead.
+import time
 import sys
 import os
 import re
 
 from .PwitchLogging import *
+from .PwitchAdmin import PwitchAdmin
 from .PwitchUtils import parent_dir, get_datetime
 
 ## Pwitch main module.
@@ -21,23 +23,22 @@ from .PwitchUtils import parent_dir, get_datetime
 
 ## Enable reconnection if interupted.
 
-class Pwitch:
+class Pwitch(PwitchAdmin):
     def __init__(self,
                 username, 
                 oauth, 
-                ircRoom,
+                channel,
                 updateRate=(10/30),
                 verbose=False,
                 host="irc.twitch.tv",
                 port=6667,
                 logging=True,
-                admin=False,
+                admin=True,                 ## Change to false.
                 userBuffer=False,
                 chatCommands=None,
                 loyaltyMode=False,
                 process_queue=None,
                 ):
-
         """
         Pwitch
         Parent Pwitch package class.
@@ -45,7 +46,7 @@ class Pwitch:
         Parameters:-
         :param username:        Twitch account username.
         :param oauth:           Twitch account OAuth token.
-        :param ircRoom:         Twitch IRC room to connect to.
+        :param channel:         Twitch IRC room to connect to.
         :param updateRate:      Limit number of connections to twitch server (per sec).
         :param verbose:         Turn on verbose output (Default: False).
         :param host:            The Twitch IRC sever (irc.twitch.tv).
@@ -53,29 +54,31 @@ class Pwitch:
         :param logging:         Toggle chat logging. Options are:
             - True (Log to log/pwitch.db).
             - FILE (log to specified file).
-        :param admin:           Enable administration of ircRoom.
+        :param admin:           Enable administration of channel.
 
         Note: Default stat_rate is 60 seconds.
         Note: updateRate default = 10/30 (30 commands per 10 secs).
         """
 
+        ## Default variable Initialisations/definitions
         self.username = username
         self.oauth = oauth
-        self.ircRoom = ircRoom
+        self.channel = channel
         self.updateRate = updateRate
         self.verbose = verbose
         self.host = host
         self.port = port
-        self.logging = logging
-        self.admin = admin
-
-        self.chatCommands = chatCommands
-        self.mod_only_mode = False
         self.sock = self.connectIRC()
         self.process_queue = process_queue
 
+        ## Optional Logging / Channel administration
+        self.logging = logging
+        self.admin = admin
+        self.chatCommands = chatCommands
+
+        ## PwitchClient variables :: Should be bundled with Client.
+        self.mod_only_mode = False      ## Should be in PwitchClient.
         self.userBuffer = userBuffer
-        self.connected = True
 
     def start(self):
         """
@@ -83,33 +86,44 @@ class Pwitch:
 
         Helper function; starts the Pwitch bot using user defined options.
         """
-
-        self._createLogDirectory()
+        ## Initial channel setup
+        self.connected = True
         self.mod_list = self.getMods()
 
-        try:
-            self.updateIRC()
-        except KeyboardInterrupt:
-            pass
+        if self.admin:
+            self.admin_object = PwitchAdmin(self.sock, self.channel,
+                self.mod_list)
+        if self.logging:
+            from .PwitchLogging import PwitchLogging
+            self._createLogDirectory()
+        if self.chatCommands:                       ## rename
+            from .PwitchAdmin import PwitchCron
 
-    def connectIRC(self, ircRoom=None):
+        #try:
+        #    self.updateIRC()
+        #except KeyboardInterrupt:
+        #    pass
+        self.updateIRC()
+
+    def connectIRC(self, channel=None):
         """
         connectIRC
 
         Initialise connection to twitch irc room using class login/oauth.
 
         Prameters:-
-        :param ircRoom:    Twitch IRC room to connect to.
+        :param channel:    Twitch IRC room to connect to.
         """
 
-        if not ircRoom:
-            ircRoom = self.ircRoom
+        if not channel:
+            channel = self.channel
+        channel = "#"+channel
 
         s = socket.socket()
         s.connect((self.host, self.port))
         s.send("PASS {}\r\n".format(self.oauth).encode("utf-8"))
         s.send("NICK {}\r\n".format(self.username).encode("utf-8"))
-        s.send("JOIN {}\r\n".format(ircRoom).encode("utf-8"))
+        s.send("JOIN {}\r\n".format(channel).encode("utf-8"))
         s.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
         s.send("CAP REQ :twitch.tv/tags\r\n".encode("utf-8"))
         s.send("CAP REQ :twitch.tv/membership\r\n".encode("utf-8"))
@@ -133,6 +147,11 @@ class Pwitch:
             sock=self.sock
 
         while self.connected:
+            try:
+                self.connected = self.process_queue.get_nowait()
+            except:
+                pass
+
             ready = select.select([sock], [], [], self.updateRate)
 
             if ready[0]:
@@ -141,7 +160,6 @@ class Pwitch:
                 continue
             if response == "PING :tmi.twitch.tv\r\n":
                 sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-
             else:
                 ## name search groups: 1: mod, 2: name, 3: chat
                 ## name searches for chat messages from users.
@@ -158,7 +176,7 @@ class Pwitch:
                             +len(self.username)+2)+'\r')
 
                     if name:
-                        if name.group(2) == self.ircRoom.lstrip('#'):
+                        if name.group(2) == self.channel:
                             print("[B] {}: {}".format(name.group(2),
                                 name.group(3)))
 
@@ -175,17 +193,23 @@ class Pwitch:
                             readline.get_line_buffer().lstrip(self.username)))
                         sys.stdout.flush()
 
+                ## Log to sqlite3 database if enabled.
                 if self.logging and name:
                     self.database.log_chat(name.group(2), get_datetime(), name.group(3))
 
+                ## Listen for user chat commands.
                 if self.admin and name:
-                    pass
+                    if re.match("^!", name.group(3)):
+                        self.admin_object.switch(name.group(3))
 
-                ## PwitchServer flag
-                try:
-                    self.connected = self.process_queue.get_nowait()
-                except:
-                    pass
+    def user_chat_command(self, command):
+        """
+        _user_chat_command
+        Handles chat commands prefixed with exclamation mark (!). 
+        
+        Parameters:-
+        :param command:         Passed chat command.
+        """
 
     def _createLogDirectory(self):
         """
@@ -203,8 +227,8 @@ class Pwitch:
                 if not os.path.exists(db_dir):
                     os.mkdir(db_path)
                 db_path = db_dir+"pwitch.db"
-            #self.database = PwitchLogging(db_path, self.ircRoom.lstrip('#'))
-            self.database = PwitchLogging(db_path, self.ircRoom.lstrip('#'))
+            #self.database = PwitchLogging(db_path, self.channel.lstrip('#'))
+            self.database = PwitchLogging(db_path, self.channel)
 
     def chatCommand(self):
         pass
@@ -261,7 +285,8 @@ class Pwitch:
         """
         if not socket:
             socket = self.sock
-        socket.send("PRIVMSG {} :{}\r\n".format(self.ircRoom, message).encode("utf-8"))
+        print(self.channel)
+        socket.send("PRIVMSG {} :{}\r\n".format(self.channel, message).encode("utf-8"))
 
     def whisper(self, user, message):
         """Send a whisper to the specified user."""
--- pwitch/PwitchAdmin.py
+++ pwitch/PwitchAdmin.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3.6
 
 import re
+import socket
+import select
+
+## Need to check whether user has permission to use command.
+## Log user commands (only if successful i.e. have correct permissions).
 
 class PwitchAdmin:
-    def __init__(self, command):
+    def __init__(self,
+                 sock,
+                 channel,
+                 mod_list,
+                ):
 
         """
         PwitchAdmin
         Pwitch plugin class - Enables use of admin commands
 
         Paramters:-
-        :param command:         Passed user command.
+        :param sock:            Socket object connected to twitch irc server.
+        :param channel:         The moderated channel.
+        :param mod_list:            List of channel mods.
         """
-
-        self.command = command
+        self.sock = sock
+        self.channel = "#"+channel
+        self.mod_list = self.mod_list
 
     def switch(self, command):
         command = re.match(r"^[\\.!](\w*)\s*(\w*)\s*(\w*)", command, re.I|re.M)
@@ -46,56 +58,43 @@ class PwitchAdmin:
 
         return function(a1, a2)
 
-    def help(self):
+    def chat(self, message, socket=None):
+        """
+        chat
+
+        Fundamental Pwitch method.
+        Sends IRC commands to Twitch.
+
+        Parameters:-
+        :param message:    Command sent to Twitch IRC servers.
+        :param socket:     Socket connection to Twitch IRC servers.
+
+        """
+        if not socket:
+            socket = self.sock
+        socket.send("PRIVMSG {} :{}\r\n".format(self.channel, message).encode("utf-8"))
+
+    def test(self, *args):
+        self.chat("This is a test!")
+
+    def help(self, *args):
         return "This is the help method."
 
     def custom(self, *command):
         return "This is the custom command"
 
-    def getMods(self):
-        """
-        getMods
-
-        Returns a list of moderators for the current channel.
-        """
-        sock = self.connectIRC()
-        n=None
-
-        ## Try loops stops script from breaking if server doesn't reply
-           
-        ## Try loops stops script from breaking if server doesn't reply
-        ## immediately
-        while not n:
-            try:
-                ready = select.select([sock], [], [], self.updateRate)
-                if ready[0]:
-                    response = sock.recv(1024).decode("utf-8")
-                else:
-                    continue
-                if response == "PING :tmi.twitch.tv\r\n":
-                    sock.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-                self.chat(".mods", socket=sock)
-                n = re.search(r':+\s(.*)', response, re.I|re.M)
-            except:
-                n = None
-
-        outlist = list(n.group().strip("\r: ").split(", "))
-        sock.close()
-
-        return outlist
-
-    def whisper(self, *arguments):
+    def whisper(self, *args):
         """Send a whisper to the specified user."""
         ## Needs implementing.
         pass
 
-    def ban(self, *user):
+    def ban(self, *args):
         """Ban the specified user from the current channel."""
-        self.chat(".ban {}".format(user))
+        self.chat(".ban {}".format(args[0]))
 
-    def unban(self, *user):
+    def unban(self, *args):
         """Unban the specified user from the current channel."""
-        self.chat(".unban {}".format(user))
+        self.chat(".unban {}".format(args[0]))
 
     def timeout(self, *args):
         """Timeout the specified user for the given number of seconds."""
@@ -109,20 +108,20 @@ class PwitchAdmin:
         """Limit how often users may send messages, in seconds."""
         self.chat(".slow {}".format(args[0]))
 
-    def slowoff(self):
+    def slowoff(self, *args):
         """Disable slowmode."""
         self.chat(".slowoff")
 
-    def clear(self):
+    def clear(self, *args):
         """Clear chatroom."""
         self.chat(".clear")
         print("Chat was cleared by a moderator")
 
-    def subscribers(self):
+    def subscribers(self, *args):
         """Turn on subscribers only mode."""
         self.chat(".subscribers")
 
-    def subscribersoff(self):
+    def subscribersoff(self, *args):
         """Turn off subscribers only mode."""
         self.chat(".subscribersoff")
 
@@ -138,7 +137,7 @@ class PwitchAdmin:
         """Host the specified channel."""
         self.chat(".host {} {}".format(args[0], " ".join(args[1:])))
 
-    def unhost(self):
+    def unhost(self, *args):
         """Stop hosting channels."""
         self.chat(".unhost")
 
@@ -149,13 +148,12 @@ class PwitchAdmin:
     def mod(self, *args):
         """Grant mod status to a user."""
         self.chat(".mod {}".format(args[0]))
+        self.mod_list.append(arg[0])
 
     def unmod(self, *args):
         """Revoke mod status from a user."""
         self.chat(".unmod {}".format(args[0]))
-
-    def test(self, *args):
-        print("This is text: {}".format(" ".join(args)))
+        self.mod_list.remove(args[0])
 
 class PwitchCron:
     def __init__(self,
